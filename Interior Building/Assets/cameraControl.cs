168422974616ae70cf68062b27f11913a088feb6
Shivan Modha <shivanmodha@gmail.com>
2018-05-15 01:17:59 -0400

    New Terrain Test

---
+++ Interior Building/Assets/cameraControl.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class cameraControl : MonoBehaviour
+{
+    public float WheelAmount = 0;
+    public float MaxZoomOut = -3.0f;
+    public float MinZoomIn = 2.0f;
+    private void Start()
+    {
+
+    }
+    private void FixedUpdate()
+    {
+        float wheel = Input.GetAxis("Mouse ScrollWheel");
+        WheelAmount += wheel;
+        if (WheelAmount > MaxZoomOut && WheelAmount < MinZoomIn)
+        {
+            transform.Translate(Vector3.forward * wheel);
+        }
+        else
+        {
+            if (WheelAmount < MaxZoomOut)
+            {
+                WheelAmount = MaxZoomOut;
+            }
+            else if (WheelAmount > MinZoomIn)
+            {
+                WheelAmount = MinZoomIn;
+            }
+        }
+    }
+}
---
+++ Interior Building/Assets/character.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+public class character : MonoBehaviour
+{
+    float vR = 0;
+	void Start ()
+    {
+        //Cursor.lockState = CursorLockMode.Locked;
+        //Cursor.visible = false;
+	}
+	void Update ()
+    {
+        float speed = 5;
+        float lookX = Input.GetAxis("Mouse X") * 2;
+        float lookY = Input.GetAxis("Mouse Y") * 2;
+        float left = Input.GetAxis("Horizontal") * speed;
+        float forward = Input.GetAxis("Vertical") * speed;
+        float jump = 0;
+
+        CharacterController cc = GetComponent<CharacterController>();
+
+        cc.transform.Rotate(new Vector3(0, lookX, 0));
+
+        Vector3 motion = new Vector3(left, 0, forward);
+        motion = cc.transform.rotation * motion;
+
+        vR -= lookY;
+        vR = Mathf.Clamp(vR, -60.0f, 90.0f + 60.0f);
+
+        Camera.allCameras[0].transform.localRotation = Quaternion.Euler(vR, 0, 0);
+
+        cc.SimpleMove(motion);
+    }
+}
